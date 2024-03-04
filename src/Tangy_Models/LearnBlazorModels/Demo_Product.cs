c6794ed1400d61b6cdb70fbf6c030c542ddcff61
sarvar-swe <s.azodov.software.engineer@gmail.com>
2024-03-04 14:00:39 +0500

    -Demo product models added

---
+++ src/Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,10 @@
+namespace Tangy_Models.LearnBlazorModels;
+
+public class Demo_Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public bool IsActive { get; set; }
+    public IEnumerable<Demo_ProductProp> ProductProps { get; set; }
+}

20e6690e2e9583abd77799af705072b9cb6ffc88
sarvar-swe <s.azodov.software.engineer@gmail.com>
2024-03-13 16:33:22 +0500

    JsRuntime Extensions

---
+++ src/TangyWeb_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,15 @@
+using Microsoft.JSInterop;
+
+namespace TangyWeb_Server.Helper;
+
+public static class IJSRuntimeExtension
+{
+    public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+    }
+    public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+    }
+}

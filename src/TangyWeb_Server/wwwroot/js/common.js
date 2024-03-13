2a83f65bda667c5ca9ed1b94440bb7f6f031ef9f
sarvar-swe <s.azodov.software.engineer@gmail.com>
2024-03-13 16:06:04 +0500

    Adding js to blazor, using toastr

---
+++ src/TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,10 @@
+window.ShowToastr = (type, message) => {
+    if(type === "success")
+    {
+        toastr.success(message, "Operation Successfull", { timeOut: 5000 })
+    }
+    if(type === "error")
+    {
+        toastr.error(message, "Operation Failed", { timeOut: 5000 })
+    }
+}
